e225ddca9b373e463a4ddfbcda6197e91ce5ca78
Ahmad Iqbal Firmansyah <ahmedeqbal101@gmail.com>
2024-11-07 18:57:02 +0700

    Progress Tambah Mahasiswa

--- resources/views/tambah_mahasiswa/index.blade.php
+++ resources/views/tambah_mahasiswa/index.blade.php
@@ -6,12 +6,13 @@
     }
 </style>
 @section('content')
+
 <div class="col-12 text-left mb-3" id="text">
     <h2><b>Tambah Data Mahasiswa</b></h2>
 </div>
 
 <div class="card-tools">
-    <button onclick="modalAction('{{ url('/') }}')" class="btn btn-success"><i class="fas fa-plus-circle"></i> Tambah Data</button> 
+    <button onclick="modalAction('{{ url('mahasiswa/create_ajax') }}')" class="btn btn-success"><i class="fas fa-plus-circle"></i> Tambah Data</button> 
     <button onclick="modalAction('{{ url('/') }}')" class="btn btn-success"><i class="fas fa-plus-circle"></i> Import Data</button> 
     <a href="{{ url('/') }}" class="btn btn-success"><i class="fas fa-plus-circle"></i> Export Data</a>
 </div>
@@ -41,9 +42,31 @@
         </table>
     </div>
 </div>
-
+<div id="myModal" class="modal fade animate shake" tabindex="-1" role="dialog" databackdrop="static" data-keyboard="false" data-width="75%" aria-hidden="true"></div>
 @endsection
 
+
 @push('js')
-<!-- Chart.js -->
+<script>
+function modalAction(url = ''){     
+    $('#myModal').load(url,function(){       
+          $('#myModal').modal('show');     }); 
+    }
+
+    var dataMahasiswa;
+    $(document).ready(function() {
+        dataMahasiswa = $('#table_mahasiswa').DataTable({
+            serverSide:true,
+            ajax: {
+                "url": "{{ url('user/list') }}",
+                "dataType": "json",
+                "type" : "POST",
+                "data" : function (d){
+                    d.mahasiswa_id = $('#mahasiswa_id').va;();
+                },
+         }
+        })
+    })
+</script>
+
 @endpush

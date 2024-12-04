d339c98044d6802b4a0d97c0d9f0290de5f56836
username Anda <arifprasojo999@email.com>
2024-12-04 12:31:03 +0700

    side bar

--- app/Http/Controllers/HistoryKompenController.php
+++ app/Http/Controllers/HistoryKompenController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class ListKompenController extends Controller
+class HistoryKompenController extends Controller
 {
     //
 }
--- app/Http/Controllers/ListKompetensiMahasiswaContoller.php
+++ app/Http/Controllers/ListKompetensiMahasiswaContoller.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 use App\Models\KompetensiModel;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+use Yajra\DataTables\Facades\DataTables;
 
 class ListKompetensiMahasiswaContoller extends Controller
 {
@@ -20,6 +22,29 @@ class ListKompetensiMahasiswaContoller extends Controller
         $activeMenu = 'listkompenmahasiswa';
         $kompetensi = KompetensiModel::all();
         return view('admin.kompetensi.index', compact('breadcrumb', 'page', 'listkompenmahasiswa', 'activeMenu'));
+    }
+
+    public function list(Request $request)
+    {
+        $kompetensi = KompetensiModel::select('id_kompetensi', 'nama_kompetensi', 'deskripsi_kompetensi');
+
+        if ($request->has('id_kompetensi')) {
+            $kompetensi->where('id_kompetensi', $request->id_kompetensi);
+        }
+
+        return DataTables::of($kompetensi)
+        ->addIndexColumn()
+        ->addColumn('aksi', function ($kompetensi) {
+            $btn = '<button onclick="modalAction(\'' . url('/kompetensi/' . $kompetensi->id_kompetensi 
+            . '/edit_ajax') . '\')" class="btn btn-warning btn-sm">Edit</button> ';
+            
+            $btn .= '<button onclick="modalAction(\'' . url('/kompetensi/' . $kompetensi->id_kompetensi 
+            . '/delete_ajax') . '\')" class="btn btn-danger btn-sm">Hapus</button> ';
+            return $btn;
+        })
+        ->rawColumns(['aksi'])
+        ->make(true);
+    
     }
 
 }

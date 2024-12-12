54f1e32fee7fb57fa6656683fd83d7ca98824d77
Ahmad Iqbal Firmansyah <ahmedeqbal101@gmail.com>
2024-12-12 22:22:28 +0700

    Fix Login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -108,12 +108,12 @@
             @csrf
             <div class="form-group">
                 <label for="username">Username</label>
-                <input type="text" id="username" name="username" placeholder="Masukkan Username" required>
+                <input type="text" id="username" name="username" placeholder="Username Anda" required>
                 <small class="error-text"></small>
             </div>
             <div class="form-group">
                 <label for="password">Password</label>
-                <input type="password" id="password" name="password" placeholder="Masukkan Password" required>
+                <input type="password" id="password" name="password" placeholder="Password Anda" required>
                 <small class="error-text"></small>
             </div>
             <button type="submit">Login</button>

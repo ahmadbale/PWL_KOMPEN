e399a43eae5f665aac628f823ba3c850bcd08b89
username Anda <arifprasojo999@email.com>
2024-12-17 10:45:57 +0700

    API JWT

--- app/Models/MahasiswaModel.php
+++ app/Models/MahasiswaModel.php
@@ -5,19 +5,29 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use illuminate\Database\Eloquent\Factories\HasMany;
+use Tymon\JWTAuth\Contracts\JWTSubject;
 use illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany as RelationsHasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
 
-class MahasiswaModel extends Authenticatable 
+class MahasiswaModel extends Authenticatable implements JWTSubject
 {
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+    public function getJWTCustomClaims()
+    {
+        return [];
+    }
+
     protected $table = 'mahasiswa';
     protected $primaryKey = 'id_mahasiswa';
 
     use HasFactory;
 
-    protected $fillable = ['nomor_induk','image','username', 'nama', 'periode_tahun','password','jam_alpha','jam_kompen','jam_kompen_selesai','id_prodi','created_at','updated_at'];
+    protected $fillable = ['nomor_induk', 'image', 'username', 'nama', 'periode_tahun', 'password', 'jam_alpha', 'jam_kompen', 'jam_kompen_selesai', 'id_prodi', 'created_at', 'updated_at'];
     protected $hidden = ["password"];
     protected $casts = ['password' => 'hashed'];
 

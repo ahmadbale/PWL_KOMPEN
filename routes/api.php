5b6e6effc9ffe40a4057d26336b9d6be6c9bb837
Ahmad Iqbal Firmansyah <ahmedeqbal101@gmail.com>
2024-12-17 15:27:31 +0700

    Api

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\LoginController;
 use App\Http\Controllers\Api\CariKompenController;
+use App\Http\Controllers\Api\HistoryKompenMahasiswaController;
 
 // Route untuk login mahasiswa
 Route::post('/mahasiswa/login', [LoginController::class, 'mahasiswaLogin']);
@@ -9,27 +10,9 @@ Route::post('/mahasiswa/login', [LoginController::class, 'mahasiswaLogin']);
 // Route untuk login personil akademik
 Route::post('/personil/login', [LoginController::class, 'personilLogin']);
 
-// Route untuk login personil akademik
-Route::post('/personil/login', [LoginController::class, 'personilLogin']);
-
-// Route untuk user profile mahasiswa
-// Route::middleware('auth:api_mahasiswa')->get('/mahasiswa', function (Request $request) {
-//     return response()->json([
-//         'success' => true,
-//         'user' => auth('api_mahasiswa')->user(),
-//     ]);
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
 // });
-
-// // Route untuk user profile personil akademik
-// Route::middleware('auth:api_personil')->get('/personil', function (Request $request) {
-//     return response()->json([
-//         'success' => true,
-//         'user' => auth('api_personil')->user(),
-//     ]);
-// });
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
 
 
 Route::prefix('kompen')->group(function () {
@@ -39,6 +22,26 @@ Route::prefix('kompen')->group(function () {
     Route::get('/{id}/show_ajax', [CariKompenController::class, 'show_ajax']); // Menampilkan detail kompen (HTML)
     Route::post('/pengajuan', [CariKompenController::class, 'store_pengajuan']); // Simpan pengajuan kompen
 });
+
+// Prefix 'histori_mahasiswa' untuk API Histori Kompen Mahasiswa
+Route::prefix('histori_mahasiswa')->group(function () {
+    // Endpoint untuk mendapatkan breadcrumb dan daftar jenis kompen
+    Route::get('/', [HistoryKompenMahasiswaController::class, 'index']);
+
+    // Endpoint untuk mendapatkan histori kompen mahasiswa
+    Route::get('/list_kompen', [HistoryKompenMahasiswaController::class, 'listKompen']);
+
+    // Endpoint untuk mendapatkan list detail kompen
+    Route::get('/list', [HistoryKompenMahasiswaController::class, 'list']);
+
+    // Endpoint untuk menampilkan detail histori kompen berdasarkan ID
+    Route::get('/{id}/show', [HistoryKompenMahasiswaController::class, 'show']);
+
+    // Endpoint untuk memperbarui progres mahasiswa berdasarkan ID
+    Route::put('/{id}/update_progres', [HistoryKompenMahasiswaController::class, 'updateProgres']);
+});
+
+
 
 
 // Route::get('cari_kompen/',[CariKompenController::class, 'index']);

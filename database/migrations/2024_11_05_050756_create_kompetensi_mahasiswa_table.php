9894d6d6e789369325577190b22db1f60b16143e
username Anda <arifprasojo999@email.com>
2024-12-22 01:03:33 +0700

    Perbaikan Migration

--- database/migrations/2024_11_05_050756_create_kompetensi_mahasiswa_table.php
+++ database/migrations/2024_11_05_050756_create_kompetensi_mahasiswa_table.php
@@ -4,12 +4,12 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateListKompetensiMahasiswaTable extends Migration
+class CreateKompetensiMahasiswaTable extends Migration
 {
     public function up()
     {
-        Schema::create('list_kompetensi_mahasiswa', function (Blueprint $table) {
-            $table->id('id_list_kompetensi_mahasiswa');
+        Schema::create('kompetensi_mahasiswa', function (Blueprint $table) {
+            $table->id('id_kompetensi_mahasiswa');
             $table->foreignId('id_mahasiswa')->constrained('mahasiswa');
             $table->foreignId('id_kompetensi')->constrained('kompetensi');
             $table->timestamps();
